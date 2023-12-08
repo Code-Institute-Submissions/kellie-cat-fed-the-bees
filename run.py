9b6e0c23b32e201fda8729088906a54394c11f92
kellie-cat <>
2023-12-08 20:16:36 +0000

    Add print_hive function and check

--- run.py
+++ run.py
@@ -6,3 +6,20 @@ PLAYER_BEE_HIVE = [[" "] * 8 for i in range(8)]
 PLAYER_VISIBLE_HIVE = [[" "] * 8 for i in range(8)]
 # Where computer guesses are displayed and where player inputs their bees
 COMPUTER_VISIBLE_HIVE = [[" "] * 8 for i in range(8)]
+
+
+def print_hive(hive):
+    print("  0  1  2  3  4  5  6  7")
+    print("  -----------------------")
+    row_number = 0
+    for row in hive:
+        print(row_number, "| ".join(row))
+        row_number += 1
+
+
+def start_game():
+    print('Check start game')
+    print_hive(PLAYER_VISIBLE_HIVE)
+
+
+start_game()
