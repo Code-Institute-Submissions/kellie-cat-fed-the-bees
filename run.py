6df21eda121af330f6d0434cb0ff5976c8032aa9
kellie-cat <>
2023-12-08 23:12:46 +0000

    Make sure the final result hive prints

--- run.py
+++ run.py
@@ -27,11 +27,12 @@ def computer_create_bees(hive):
     """
     for bee in range(NUM_BEES):
         bee_row, bee_column = randint(0, 7), randint(0, 7)
-        hive[bee_row][bee_column] = "X"
+        hive[bee_row][bee_column] = "0"
         print(bee_row, bee_column)
 
 
 def guess_bee_location(hive):
+    guess_list = []
     for turn in range(5):
         while True:
             try:
@@ -50,27 +51,35 @@ def guess_bee_location(hive):
             except ValueError:
                 print('Guess not valid, try again')
         return guess_row, guess_column
+        guess_coordinates = guess_row, guess_column
+        if guess_coordinates in guess_list:
+            print("You already tried there, try again")
+            turn -= 1
+        guess_list.append(guess_coordinates)
 
 
 def start_game():
     computer_create_bees(PLAYER_BEE_HIVE)
     print_hive(PLAYER_VISIBLE_HIVE)
+    print('Introduction')
+    print('Instructions')
     for turn in range(0, 5, 1):
         while True:
             print('Guess a bee location')
             print_hive(PLAYER_VISIBLE_HIVE)
             guess_row, guess_column = guess_bee_location(PLAYER_BEE_HIVE)
             if PLAYER_VISIBLE_HIVE[guess_row][guess_column] == "-":
-                print("You guessed that one already.")
-            elif PLAYER_BEE_HIVE[guess_row][guess_column] == "X":
-                print("Hit")
-                PLAYER_VISIBLE_HIVE[guess_row][guess_column] = "X"
+                print("You guessed that one already, have another try.")
+            elif PLAYER_BEE_HIVE[guess_row][guess_column] == "0":
+                print("You fed the bee!")
+                PLAYER_VISIBLE_HIVE[guess_row][guess_column] = "0"
                 break
             else:
-                print("MISS!")
+                print("MISS! The bees are still hungry")
                 PLAYER_VISIBLE_HIVE[guess_row][guess_column] = "-"
                 break
             turn += 1
+    print_hive(PLAYER_VISIBLE_HIVE)
     print('Show results')
 
 
