35463f655f6824d675ec5a662bb50c6b28eaba2e
kellie-cat <>
2023-12-08 20:12:21 +0000

    Import random, add hives as global variables

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+# Where computer places their bees randomly
+PLAYER_BEE_HIVE = [[" "] * 8 for i in range(8)]
+# Where players guesses are displayed
+PLAYER_VISIBLE_HIVE = [[" "] * 8 for i in range(8)]
+# Where computer guesses are displayed and where player inputs their bees
+COMPUTER_VISIBLE_HIVE = [[" "] * 8 for i in range(8)]
