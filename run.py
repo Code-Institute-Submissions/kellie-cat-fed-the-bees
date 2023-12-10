76d82864a787731ae3c6a1976c1f03da63915a6b
kellie-cat <>
2023-12-10 19:51:43 +0000

    Give result ton player

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ PLAYER_VISIBLE_HIVE = [[" "] * 8 for i in range(SIZE)]
 def print_intro():
     title_art = pyfiglet.figlet_format('FREE THE BEES',
                                        font="bubble")
-    print(title_art)
+    print(Fore.YELLOW + (title_art) + Fore.RESET)
     input("Press enter to start the game...\n")
     print("In a dystopian time, bees are hungry and can't escape their hive")
     print('You have stumbled across a beehive and want to help')
@@ -26,9 +26,11 @@ def print_intro():
     print(f'The hive is {SIZE} squares long and high')
     print(f'There are {NUM_BEES} bees to find in each hive')
     print('Feed as many bees as you can so they can survive!')
-    print('Feed the bees by guessing a coordinate')
+    print(f'You only have {TURNS} drops of nectar to give them')
+    print('Feed the bees by guessing a coordinate you think they are at')
     print('When prompted, input a number for x-axis (horizontal rows) first')
     print('Then a number for the y-axis (vertical columns)')
+    input('Press enter to FEED THE BEES...\n')
 
 
 def print_hive(hive):
@@ -55,7 +57,6 @@ def computer_create_bees(hive):
 
 
 def guess_bee_location(hive):
-    guess_list = []
     for turn in range(TURNS):
         while True:
             try:
@@ -74,23 +75,17 @@ def guess_bee_location(hive):
             except ValueError:
                 print('Guess not valid, try again')
         return guess_row, guess_column
-        guess_coordinates = guess_row, guess_column
-        if guess_coordinates in guess_list:
-            print("You already tried there, try again")
-            turn -= 1
-        guess_list.append(guess_coordinates)
 
 
 def start_game():
+    success = 0
     computer_create_bees(PLAYER_BEE_HIVE)
-    print_hive(PLAYER_VISIBLE_HIVE)
 
     print_intro()
 
-    print('Instructions')
     for turn in range(0, TURNS, 1):
         while True:
-            print('Guess a bee location')
+            print('Guess a bee location on the hive below...')
             print_hive(PLAYER_VISIBLE_HIVE)
             guess_row, guess_column = guess_bee_location(PLAYER_BEE_HIVE)
             if PLAYER_VISIBLE_HIVE[guess_row][guess_column] == "-":
@@ -102,6 +97,7 @@ def start_game():
             elif PLAYER_BEE_HIVE[guess_row][guess_column] == "X":
                 print(Fore.GREEN + "You fed the bee!" + Fore.RESET)
                 PLAYER_VISIBLE_HIVE[guess_row][guess_column] = "0"
+                success += 1
                 break
             else:
                 print(Fore.RED + "MISS! The bees are still hungry" +
@@ -110,7 +106,7 @@ def start_game():
                 break
             turn += 1
     print_hive(PLAYER_VISIBLE_HIVE)
-    print('Show results')
+    print(f'Well done for feeding {success} bees!')
 
 
 start_game()
